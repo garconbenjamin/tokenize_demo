324b72fd2c6f1a9bdefdb041cc75f32f180537aa
Chin-yu Hsu <chinyu.official@gmail.com>
2023-08-17 01:54:54 +0800

    chore: update babel and eslint config to support alias path.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import * as eva from '@eva-design/eva';
+import {ApplicationProvider} from '@ui-kitten/components';
+import Home from '@src/screens/Home';
+
+function App() {
+  return (
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <Home />
+    </ApplicationProvider>
+  );
+}
+
+export default App;
