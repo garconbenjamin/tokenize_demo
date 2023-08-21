f90cda73d7775751e38f704a85e00e5943964ca9
Chin-yu Hsu <chinyu.official@gmail.com>
2023-08-22 03:10:59 +0800

    refactor: Move providers to index level

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,38 @@
-import React from 'react';
-import '@/i18n';
-import 'react-native-gesture-handler';
-import {Provider as ReduxProvider} from 'react-redux';
-import {ThemeProvider} from '@rneui/themed';
+import React, {useEffect} from 'react';
+
 import {NavigationContainer} from '@react-navigation/native';
-import 'react-native-gesture-handler';
+
 import {createStackNavigator} from '@react-navigation/stack';
 import {RootScreenType, RootStackParamList} from '@/types/navigators';
 import Auth from '@/screens/Auth';
 import TabNavigator from '@/navigators/Tab';
-import theme from '@/theme';
-import store from '@/store';
-
+import {getUniqueId, getUserAgent} from 'react-native-device-info';
+import {setConfig} from '@/store/actions';
 const RootStack = createStackNavigator<RootStackParamList>();
 
 function App() {
+  useEffect(() => {
+    const intializeConfig = async () => {
+      const userAgent = await getUserAgent();
+      const deviceId = await getUniqueId();
+      setConfig({userAgent, deviceId});
+    };
+    intializeConfig();
+  }, []);
   return (
-    <ReduxProvider store={store}>
-      <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <RootStack.Navigator
-            screenOptions={{
-              headerShown: false,
-            }}
-            id="Root">
-            <RootStack.Screen
-              name={RootScreenType.AUTHENTICATION}
-              component={Auth}
-            />
-            <RootStack.Screen
-              name={RootScreenType.MAIN}
-              component={TabNavigator}
-            />
-          </RootStack.Navigator>
-        </NavigationContainer>
-      </ThemeProvider>
-    </ReduxProvider>
+    <NavigationContainer>
+      <RootStack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        id="Root">
+        <RootStack.Screen
+          name={RootScreenType.AUTHENTICATION}
+          component={Auth}
+        />
+        <RootStack.Screen name={RootScreenType.MAIN} component={TabNavigator} />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
 
